77c47333ceb6c9966462a7259e77c817c335d0e5
tapan288 <tapansharma69@gmail.com>
2023-08-04 13:18:43 +0545

    define Blog and Tag models along with pivot table

---
+++ database/migrations/2023_08_04_073126_create_blog_tag_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blog_tag', function (Blueprint $table) {
+            $table->foreignId('blog_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blog_tag');
+    }
+};
