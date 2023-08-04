06ecd88fc9b2c480bd1544bf6c0f1b848a077049
tapan288 <tapansharma69@gmail.com>
2023-08-04 13:11:56 +0545

    setup project for new blog

--- resources/js/Pages/Home.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import { Link } from "@inertiajs/svelte";
-</script>
-
-<h1>Hello from Home</h1>
-<Link href={route("welcome")}>Welcome</Link>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'index'])->name('posts.index');
-Route::post('/', [PostController::class, 'store'])->name('posts.store');
-
-Route::get('/home', function () {
-    return Inertia::render('Home');
-})->name('home');
+Route::get('/', function () {
+    dd('sdhcsds');
+})->name('blogs.index');
